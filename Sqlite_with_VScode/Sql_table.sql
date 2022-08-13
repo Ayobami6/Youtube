8e4b22d5690ba838716f763d0f6631ce22a14ee3
Ayo <ayobamidele006@gmail.com>
2022-08-13 08:59:20 +0100

    add files

---
+++ Sqlite_with_VScode/Sql_table.sql
@@ -0,0 +1,12 @@
+-- SQLite
+CREATE TABLE new
+(name varchar(50),
+id int,
+address varchar(100),
+phone_no varchar(50))
+
+INSERT into new VALUES
+('Ayo', 123, 'ojota', '+2347033453526'),
+('Tunde', 124, 'Fadeyi', '+2347033457826')
+
+SELECT * FROM new
